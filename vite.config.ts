82ea6dbfda45579518b28ab518769c3526cc4333
yoonsejong <oxwk123@gmail.com>
2024-10-30 17:57:03 +0900

    feat 보안패치 오버레이 false 설정

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    hmr: {
+      overlay: false, // 에러 오버레이 비활성화
+    },
+  },
 })
