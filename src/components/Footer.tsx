5cb4e73e5c39d5f3121f57d3c47349a875db1ccb
yoonsejong <oxwk123@gmail.com>
2025-03-16 23:43:27 +0900

    FIX footer 변경

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer: React.FC = () => {
     return (
         <footer className="bg-gray-200 dark:bg-gray-800 text-gray-600 dark:text-gray-400 text-sm py-2">
             <div className="container mx-auto text-center">
-                <p>&copy; 2024 KeyWork. All rights reserved.</p>
+                <p>&copy; 2025 KeyWork. All rights reserved.</p>
             </div>
         </footer>
     );
